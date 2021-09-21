b93c99e8fefd53730282a9975e72cd4883f02983
bpavel07@yandex.ru <gggtttttHHHHHH7889787>
2021-09-21 17:40:39 +0300

    111

--- src/JavaApplication23.java
+++ src/JavaApplication23.java
@@ -1,6 +1,25 @@
+import java.util.Random;
+
 public class JavaApplication23 {
 
     public static void main(String[] args) {
+        GlobalData.array = new int[GlobalData.ARRAY_LENGTH];
+        GlobalData.results = new long[GlobalData.THREAD_COUNT];
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < GlobalData.ARRAY_LENGTH; i++){
+            GlobalData.array[i] = rnd.nextInt(10);
+        }
+
+        long sum1 = 0;
+        long t1 = System.currentTimeMillis();
+        for(int i =0; i< GlobalData.ARRAY_LENGTH; i++){
+            sum1 += GlobalData.array[i];
+        }
+        long t2 = System.currentTimeMillis();
+
+        System.out.println("sum1 = " + sum1+ ", working time = " + (t2-t1));
 
     }
 }
--- src/SumThread.java
+++ src/SumThread.java
@@ -1,6 +1,17 @@
 public class SumThread extends Thread {
 
-    
+    private int pos;
 
+    public SumThread(int pos){
+        this.pos =pos;
+    }
 
+    @Override
+    public void run() {
+
+        for(int i= pos; i< GlobalData.ARRAY_LENGTH; i += GlobalData.THREAD_COUNT){
+            GlobalData.results[pos] += GlobalData.array[i];
+        }
+
+    }
 }
